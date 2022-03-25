93e434a2cc6fc3629c65ce6834ebf946366f3cd5
erwin <winzon353535@gmail.com>
2022-03-25 10:33:31 -0700

    add models for user, clown, and booking

---
+++ db/migrate/20220325173316_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.integer :user_id
+      t.integer :clown_id
+      t.string :time
+      t.string :location
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
