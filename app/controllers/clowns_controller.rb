69505477de2faec3bb249b94b8d2e939bf02d113
erwin <winzon353535@gmail.com>
2022-03-25 10:50:38 -0700

    added routes and controller + actions for clown

---
+++ app/controllers/clowns_controller.rb
@@ -0,0 +1,33 @@
+class ClownsController < ApplicationController
+
+  def index
+    clowns = Clown.all
+    render json: clowns
+  end
+
+  def show
+    clown = Clown.find_by(id: params[:id])
+    render json: clown
+  end
+  
+  def create
+    clown = Clown.new(
+      name: params[:name]
+    )
+    clown.save
+    render json: clown
+  end
+  
+  def update
+    clown = Clown.find_by(id: params[:id])
+    clown.name = params["name"] || clown.name
+    render json: clown
+  end
+  
+  def destroy
+    clown = Clown.find_by(id: params[:id])
+    clown.delete
+    render json: {message: "Clown FIRED"}
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Clowns
+  get "/clowns" => "clowns#index"
+  get "/clowns/:id" => "clowns#show"
+  post "/clowns" => "clowns#create"
+  patch "/clowns/:id" => "clowns#update"
+  delete "/clowns/:id" => "clowns#destroy" #fire a clown
+
 end
